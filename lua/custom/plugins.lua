be805d7e2348334f411cf50047b4c231b22f7c2b
douglas <douglasmaxton@gmail.com>
2024-02-28 11:51:07 +0000

    reorganised custom plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,9 +1,120 @@
 -- Order of plugins
--- Most important at the top
--- Plugins that will be lazy loaded false
--- All other Plugins after that
+-- Most important at the top: language server protocals and plugin managers
+-- Plugins that change neovim functionality
+-- Plugins used for testing and debugging
+-- Plugins that give neovim extra function but is loaded for all filetypes
+-- Third party plugin that are loaded based on filetypes: this will be in alphabitical order
 
 local plugins = {
+  {
+    -- Description: this plugin is used to populate the popup window
+    --   for faster code executions
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    -- Description: this is used as a light weight plugin manager
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff",
+        "pyright",
+      }
+    }
+  },
+  {
+    -- Description: use neovim as a language server to inject LSP diagnostics, code actions, and more via lua
+    --  This repo has been archived by the owner
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = { "python" },
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    -- Description: This help with autosaving, it will autosave
+    --   on InsertLeave and TextChanged
+    "0x00-ketsu/autosave.nvim",
+    lazy = false,
+    config = function()
+      local autosave = require("autosave")
+      autosave.setup({
+        enable = true,
+        prompt_style = 'stdout',
+        prompt_message = function()
+          return 'Autosave: saved at ' .. vim.fn.strftime('%H:%M:%S')
+        end,
+        events = { 'InsertLeave', 'TextChanged' },
+        conditions = {
+          exists = true,
+          modifiable = true,
+          filename_is_not = {},
+          filetype_is_not = {},
+        },
+        write_all_buffers = false,
+        debounce_delay = 135
+      })
+    end
+  },
+  {
+    -- Description: This is used to store session based on the directory that I am in
+    --   in will save session on save
+    "shatur/neovim-session-manager",
+    lazy = false,
+    config = function()
+      local Path = require("plenary.path")
+      local config = require("session_manager.config")
+      require("session_manager").setup({
+        session_dir = Path:new(vim.fn.stdpath('data'), 'session'),
+        -- session_filename_to_dir = session_filename_to_dir,
+        autoload_mode = config.AutoloadMode.CurrentDir,
+        autosave_last_session = true,
+        autosave_ignore_not_normal = true,
+        autosave_ignore_dirs = {},
+        autosave_ignore_filetypes = {
+          'gitcommit',
+          'gitrebase',
+          'snippets',
+        },
+        autosave_ignore_buftypes = {},
+        autosave_only_in_session = false,
+        max_path_length = 80,
+      })
+    end
+  },
+  {
+    -- Description: Keeps the cursor centered on the screen
+    --   in a window pane
+    "arnamak/stay-centered.nvim",
+    lazy = false,
+    config = function()
+      require("stay-centered").setup()
+    end,
+  },
+  {
+    -- Description: I use this plugin to close all hidden buffers
+    --   using and auto command
+    "kazhala/close-buffers.nvim",
+    lazy = false,
+  },
+  {
+    -- Description: This plugin will handle all git commands
+    --   using ctrl+g it opens up a new terminal for git
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    config = function()
+      require("core.utils").load_mappings("lazygit")
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    }
+  },
   {
     -- Description: This plugin is used to autoformat code on save
     --   using the event defined below
@@ -41,97 +152,11 @@ local plugins = {
     end
   },
   {
-    -- Description: I use this plugin to close all hidden buffers
-    --   using and auto command
-    "kazhala/close-buffers.nvim",
-    lazy = false,
-  },
-  {
-    -- Description: Keeps the cursor centered on the screen
-    --   in a window pane
-    "arnamak/stay-centered.nvim",
-    lazy = false,
+    -- Description: this is used for debugging and testing code
+    --   using breakpoints and other functionality
+    "mfussenegger/nvim-dap",
     config = function()
-      require("stay-centered").setup()
-    end,
-  },
-  {
-    -- Description: This will open a Markdown or Readme file in a new browser tab
-    --   I have set an auto command to open automatically
-    --   If I close the markdown or readme file in question it will auto close browser tab
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_theme = 'light'
-      vim.g.mkdp_auto_start = 1
-    end,
-    config = function()
-      vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
-    end
-  },
-  {
-    -- Description: This is used to store session based on the directory that I am in
-    --   in will save session on save
-    "shatur/neovim-session-manager",
-    lazy = false,
-    config = function()
-      local Path = require("plenary.path")
-      local config = require("session_manager.config")
-      require("session_manager").setup({
-        session_dir = Path:new(vim.fn.stdpath('data'), 'session'),
-        -- session_filename_to_dir = session_filename_to_dir,
-        autoload_mode = config.AutoloadMode.CurrentDir,
-        autosave_last_session = true,
-        autosave_ignore_not_normal = true,
-        autosave_ignore_dirs = {},
-        autosave_ignore_filetypes = {
-          'gitcommit',
-          'gitrebase',
-          'snippets',
-        },
-        autosave_ignore_buftypes = {},
-        autosave_only_in_session = false,
-        max_path_length = 80,
-      })
-    end
-  },
-  {
-    -- Description: This plugin will handle all git commands
-    --   using ctrl+g it opens up a new terminal for git
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-    config = function()
-      require("core.utils").load_mappings("lazygit")
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    }
-  },
-  {
-    -- Description: This help with autosaving, it will autosave
-    --   on InsertLeave and TextChanged
-    "0x00-ketsu/autosave.nvim",
-    lazy = false,
-    config = function()
-      local autosave = require("autosave")
-      autosave.setup({
-        enable = true,
-        prompt_style = 'stdout',
-        prompt_message = function()
-          return 'Autosave: saved at ' .. vim.fn.strftime('%H:%M:%S')
-        end,
-        events = { 'InsertLeave', 'TextChanged' },
-        conditions = {
-          exists = true,
-          modifiable = true,
-          filename_is_not = {},
-          filetype_is_not = {},
-        },
-        write_all_buffers = false,
-        debounce_delay = 135
-      })
+      require("core.utils").load_mappings("dap")
     end
   },
   {
@@ -155,14 +180,6 @@ local plugins = {
     end
   },
   {
-    -- Description: this is used for debugging and testing code
-    --   using breakpoints and other functionality
-    "mfussenegger/nvim-dap",
-    config = function()
-      require("core.utils").load_mappings("dap")
-    end
-  },
-  {
     -- Description: this is an adaptor for python with nvim-dap plugin
     --   giving me support for the python language protocals
     "mfussenegger/nvim-dap-python",
@@ -178,36 +195,22 @@ local plugins = {
     end,
   },
   {
-    -- Description: use neovim as a language server to inject LSP diagnostics, code actions, and more via lua
-    --  This repo has been archived by the owner
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = { "python" },
-    opts = function()
-      return require "custom.configs.null-ls"
+    -- Description: This will open a Markdown or Readme file in a new browser tab
+    --   I have set an auto command to open automatically
+    --   If I close the markdown or readme file in question it will auto close browser tab
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+    -- Details: I had todo npm install in the directory where this plugin was installed
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_theme = 'light'
+      vim.g.mkdp_auto_start = 1
     end,
-  },
-  {
-    -- Description: this is used as a light weight plugin manager
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "black",
-        "debugpy",
-        "mypy",
-        "ruff",
-        "pyright",
-      }
-    }
-  },
-  {
-    -- Description: this plugin is used to populate the popup window
-    --   for faster code executions
-    "neovim/nvim-lspconfig",
     config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  }
+      vim.keymap.set("n", "<Leader>mp", "<Plug>MarkdownPreview", { desc = "Markdown Preview" })
+    end
+  },
 }
 
 return plugins
